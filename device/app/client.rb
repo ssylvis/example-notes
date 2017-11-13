292d21a6d4b0227204cbeab06d28a9b8f23644f0
Sean Sylvis <ssylvis@gmail.com>
2017-11-13 14:42:57 -0800

    Use ruby-concurrent TimerTask for client heartbeating

--- device/app/client.rb
+++ device/app/client.rb
@@ -1,21 +1,37 @@
-require 'grpc'
+require 'concurrent'
 require 'device_services_pb'
+require 'grpc'
 require 'securerandom'
 
 module Client
+  class Application
+    def initialize(host, port, uid = Nil)
+      @device_uid = uid || SecureRandom.uuid
+      @device_stub = Backend::Device::Stub.new("#{host}:#{port}", :this_channel_is_insecure)
+    end
+
+    def heartbeat
+      request = Backend::HeartbeatRequest.new(
+        device_uid: @device_uid,
+        timestamp: Time.now.to_i)
+      response = @device_stub.heartbeat(request)
+      puts "Heartbeat: #{response.to_hash}"
+    rescue => e
+      puts "Exception on heartbeat: #{e}"
+    end
+
+    def run
+      task = Concurrent::TimerTask.new(execution_interval: 5) do
+        heartbeat
+      end
+      task.execute
+      task.wait_for_termination
+    end
+  end
+
   def self.main(device_uid)
     config = Config.instance.config['device']
-    host = config['host']
-    port = config['port']
-    uid = device_uid || SecureRandom.uuid
-
-    device_stub = Backend::Device::Stub.new("#{host}:#{port}", :this_channel_is_insecure)
-
-    request = Backend::HeartbeatRequest.new(
-      device_uid: uid,
-      timestamp: Time.now.to_i)
-
-    response = device_stub.heartbeat(request)
-    puts "Heartbeat: #{response.to_hash}"
+    app = Application.new(config['host'], config['port'], device_uid)
+    app.run
   end
 end
