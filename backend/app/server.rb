e13f29f24665a89ffd2f5cd9668fe6ab9cf2f709
Sean Sylvis <ssylvis@gmail.com>
2017-11-14 16:34:10 -0800

    Add logging

--- backend/app/server.rb
+++ backend/app/server.rb
@@ -1,13 +1,36 @@
 require 'active_record'
+require 'grpc'
 require 'device'
 require 'device_services_pb'
-require 'grpc'
 
 module Server
+  $log = Logger.new(STDOUT)
+
+  class Application
+    def initialize(config)
+      $log.info(config)
+
+      @port = config['server']['port']
+
+      # Connect to database
+      ActiveRecord::Base.establish_connection(config['database'])
+    end
+
+    def run
+      # Start server
+      server = GRPC::RpcServer.new
+      server.add_http2_port("localhost:#{@port}", :this_port_is_insecure)
+      server.handle(Server::DeviceService)
+      $log.info("Starting RpcServer at localhost:#{@port}")
+
+      server.run
+    end
+  end
+
   # Implementation of the device RPC service.
   class DeviceService < Backend::Device::Service
     def heartbeat(request, _call)
-      puts "Heartbeat: #{request.to_hash}"
+      $log.info("Heartbeat: #{request.to_hash}")
 
       # Update device heartbeat
       device = Model::Device.find_by_uid(request.device_uid)
@@ -19,7 +42,7 @@ module Server
 
       Backend::HeartbeatResponse.new
     rescue => e
-      puts "Exception thrown: #{e}"
+      $log.error("Exception thrown", e)
       Backend::HeartbeatResponse.new
     end
   end
@@ -27,18 +50,7 @@ module Server
   # main starts an RpcServer that receives requests to DeviceServer.
   def self.main
     config = Config.instance.config
-    puts config
-
-    # Connect to database
-    ActiveRecord::Base.establish_connection(config['database'])
-
-    # Start server
-    server = GRPC::RpcServer.new
-    port = config['server']['port']
-    server.add_http2_port("localhost:#{port}", :this_port_is_insecure)
-    server.handle(DeviceService)
-    puts "Starting RpcServer at localhost:#{port}"
-
-    server.run
+    app = Application.new(config)
+    app.run
   end
 end
